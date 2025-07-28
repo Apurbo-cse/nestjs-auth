b344ff8a2adaf4cb2ac83ce52f68b96e796f8541
Apurbo Kumar Anup <apurboka@gmail.com>
2025-07-28 20:13:01 +0600

    Update active-user.decorator.ts

--- src/auth/decorators/active-user.decorator.ts
+++ src/auth/decorators/active-user.decorator.ts
@@ -4,6 +4,5 @@ import { ActiveUserType } from "../interfaces/active-user-type.interface";
 export const ActiveUser = createParamDecorator((feild: keyof ActiveUserType | undefined, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest()
     const user: ActiveUserType = request.user
-
     return feild ? user?.[feild] : user
-})
+}) 

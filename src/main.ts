7be58c4b9156de25a02475ce90657c39960506ca
Apurbo Kumar Anup <apurboka@gmail.com>
2025-07-28 20:23:26 +0600

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-    transformOptions: {
-      enableImplicitConversion: true
-    }
-  }))
+
+  // Global validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Nest Auth')
+    .setDescription('Auto-generated Swagger documentation')
+    .setVersion('1.0')
+    .addBearerAuth() // Optional: for JWT token support
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // Swagger at /api-docs
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
